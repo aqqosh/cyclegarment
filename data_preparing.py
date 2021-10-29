2276c4012433e0880fa8ec0d7be4215d99b6e13a
aqqosh <the.white.nurse@gmail.com>
2021-10-29 15:00:52 +0000

    minor changes

--- data_preparing.py
+++ data_preparing.py
@@ -5,18 +5,16 @@ from keras.preprocessing.image import img_to_array
 from keras.preprocessing.image import load_img
 from numpy import savez_compressed
  
-# load all images in a directory into memory
+
 def load_images(path, size=(256,256)):
-	data_list = list()
-	# enumerate filenames in directory, assume all are images
-	for filename in listdir(path):
-		# load and resize the image
-		pixels = load_img(path + filename, target_size=size)
-		# convert to numpy array
-		pixels = img_to_array(pixels)
-		# store
-		data_list.append(pixels)
-	return asarray(data_list)
+    data_list = list()
+    for filename in listdir(path):
+        pixels = load_img(path + filename, target_size=size)
+        # convert to numpy array
+        pixels = img_to_array(pixels)
+        # store
+        data_list.append(pixels)
+    return asarray(data_list)
  
 if __name__ == "__main__":
     # dataset path
